53077b659ecec733234f04e8fa35cc6bbefd7f93
Honahuku <tech@honahuku.com>
2024-08-25 21:56:07 +0900

    feat: 複数ユーザーの場合も github の鍵が存在するか確認する

--- update-ssh-keys/update_sshkeys_multiuser.sh
+++ update-ssh-keys/update_sshkeys_multiuser.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 set -Ceu
 
-# ユーザーのホームディレクトリのパスを取得
-USER_HOME=$HOME
-
 # .sshディレクトリとauthorized_keysファイルが存在しない場合は作成
-mkdir -p "$USER_HOME"/.ssh
-touch "$USER_HOME"/.ssh/authorized_keys
+mkdir -p "$HOME"/.ssh
+touch "$HOME"/.ssh/authorized_keys
 
 # .sshディレクトリとauthorized_keysファイルのパーミッションを設定
-chmod 700 "$USER_HOME"/.ssh
-chmod 600 "$USER_HOME"/.ssh/authorized_keys
+chmod 700 "$HOME"/.ssh
+chmod 600 "$HOME"/.ssh/authorized_keys
 
 # GitHubユーザー名を設定
 GITHUB_USERNAME_01="honahuku"
@@ -18,6 +15,19 @@ GITHUB_USERNAME_02="njm2360"
 GITHUB_USERNAME_03="cobalt1024"
 
 # GitHubからSSH公開鍵を取得してauthorized_keysを更新
-wget -O - "https://github.com/${GITHUB_USERNAME_01}.keys" >| "$USER_HOME"/.ssh/authorized_keys
-wget -O - "https://github.com/${GITHUB_USERNAME_02}.keys" >> "$USER_HOME"/.ssh/authorized_keys
-wget -O - "https://github.com/${GITHUB_USERNAME_03}.keys" >> "$USER_HOME"/.ssh/authorized_keys
+set +x
+KEYS=$(wget -O - "https://github.com/${GITHUB_USERNAME_01}.keys")
+KEYS+=$(wget -O - "https://github.com/${GITHUB_USERNAME_02}.keys")
+KEYS+=$(wget -O - "https://github.com/${GITHUB_USERNAME_03}.keys")
+
+# KEYS が空でない場合のみ authorized_keys を書き換え
+# FIXME: GITHUB_USERNAME に指定した3つのうちいずれかが空だったとしても1つでも値が入っていれば通るかも
+# set -Ceu してるので wget が落ちたら止まるはずだが別の方法も用意しておきたい
+
+if [ -n "$KEYS" ]; then
+    echo "$KEYS" >| "$HOME"/.ssh/authorized_keys
+else
+    echo "ERROR: 公開鍵を取得出来ませんでした"
+    exit 1
+fi
+set -x
